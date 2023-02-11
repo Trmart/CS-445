30468e34e20c5533e8a32097167dc7eed1c030c1
Taylor <mart8517@vandals.uidaho.edu>
2023-02-10 16:40:14 -0800

    started working on ExpressionNodes.hpp, added ExpressionNodes.cpp, and DeclarationNodes.cpp

---
+++ hw2/src/ast/DeclarationNodes.cpp
@@ -0,0 +1,12 @@
+/*
+Taylor Martin
+CS-445 Compiler Design
+University Of Idaho
+HW2
+Dr. Wilder
+1/30/2023
+
+FILE: ExprssionNodes.hpp
+DESC: ExpressionNodes Subclass. Inherits from Node Base Class. 
+
+*/
--- hw2/src/ast/ExpressionNodes.hpp
+++ hw2/src/ast/ExpressionNodes.hpp
@@ -17,12 +17,95 @@ DESC: ExpressionNodes Subclass. Inherits from Node Base Class.
 
 #include "Node.hpp"
 //Assignment, Binary, Call, Constant, Identifier, Unary, UnaryAssignment
+
+//**********************************************************
+// ************ ExpressionNode Class ************************
+//**********************************************************
 class ExpressionNode : public Node
 {
     public :
 
     protected :
-}; 
+};
+
+
+//******************************************************
+// ************ AssignmentNode Class *******************
+//******************************************************
+class AssignmentNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ BinaryNode Class ***************************
+//**********************************************************
+class BinaryNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ CallNode Class *****************************
+//**********************************************************
+class CallNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ ConstantNode Class *************************
+//**********************************************************
+class ConstantNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ IdentifierNode Class ************************
+//**********************************************************
+class IdentifierNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ UnaryNode Class ****************************
+//**********************************************************
+class UnaryNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
+//**********************************************************
+// ************ UnaryAssignmentNode Class ******************
+//**********************************************************
+class UnaryAssignmentNode : public ExpressionNode
+{
+    public :
+
+    protected :
+};
+
+
 
 
 #endif
