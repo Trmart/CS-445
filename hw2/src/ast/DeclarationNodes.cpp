f5c64411e98d7b7a517b5ed85ad6e65b37ef59c2
Taylor <mart8517@vandals.uidaho.edu>
2023-02-11 13:50:13 -0800

    almost finished DeclarartionNodes.cpp

--- hw2/src/ast/DeclarationNodes.cpp
+++ hw2/src/ast/DeclarationNodes.cpp
@@ -11,3 +11,180 @@ DESC: DeclarationNodes Subclass Member Definitions.
 */
 
 #include "DeclarationNodes.hpp"
+
+//******************************************************
+// ************ PrimitiveType Class *******************
+//******************************************************
+PrimitiveType::PrimitiveType(const PrimitiveType::Type type, const bool isArray) : m_type(type), m_isArray(isArray)
+{
+    //set next to null
+    m_next = nullptr;
+    
+    //check if array
+    if (isArray)
+    {
+        //create new PrimitiveType
+        m_next = new PrimitiveType(type);
+        //set type to void
+        m_type = Type::VOID;
+    }
+    //not array
+    else
+    {
+        //set type
+        m_type = type;
+    }
+}
+
+std::string PrimitiveType::printTokenString() const
+{
+    std::string tokenOutputString;
+    switch(m_type)
+    {
+        case Type::INT:
+                    {
+                        tokenOutputString = "int";
+                    }
+                    break;
+
+        case Type::CHAR:
+                    {
+                        tokenOutputString = "char";
+                    }
+                    break;
+        
+        case Type::BOOL:
+                    {
+                        tokenOutputString = "bool";
+                    }
+                    break;
+
+
+        case Type::VOID:
+                    {
+                        tokenOutputString = "void";
+                    }
+                    break;
+        default:
+            {
+                tokenOutputString += "ERROR. Could not determine PrimitiveType::Type.";
+            }
+            break;
+    }
+    return tokenOutputString;
+}
+
+//setPrimitiveType
+void PrimitiveType::setPrimitiveType(const PrimitiveType::Type type)
+{
+    //set type
+    m_type = type;
+
+    //check if next is not null
+    if (m_next != nullptr)
+    {
+        //set next type
+        m_next->setPrimitiveType(type);
+    }
+}
+
+//getters
+
+//getPrimitiveType
+PrimitiveType::Type PrimitiveType::getPrimitiveType() const
+{
+    return m_type;
+}
+
+//getIsArray
+bool PrimitiveType::getIsArray() const
+{
+    return m_isArray;
+}
+
+//******************************************************
+// ************ FunctionNode Class *********************
+//******************************************************
+
+//Constructor
+FunctionNode::FunctionNode(int tokenLineNumber, const std::string functionName, PrimitiveType *returnType) : Node::Node(tokenLineNumber,functionName), m_returnType(returnType)
+{
+
+}
+
+//printTokenString
+std::string FunctionNode::printTokenString() const
+{
+    return "Func: " + m_tokenData.stringValue + " returns type " + m_returnType->printTokenString();
+}
+
+//******************************************************
+// ************ ParameterNode Class ********************
+//******************************************************
+
+//Constructor
+ParameterNode::ParameterNode(int tokenLineNumber, const std::string parameterName, PrimitiveType *parameterType) : Node::Node(tokenLineNumber,parameterName), m_parameterType(parameterType)
+{
+
+}
+
+//printTokenString
+std::string ParameterNode::printTokenString() const
+{
+    if (m_parameterType->getIsArray())
+    {
+        return "Param: " + m_tokenData.stringValue + " of array of type " + m_parameterType->printTokenString();
+    }
+    else
+    {
+        return "Param: " + m_tokenData.stringValue + " of type " + m_parameterType->printTokenString();
+    }
+}
+
+//setParameterType
+void ParameterNode::setParameterType(PrimitiveType::Type parameterType)
+{
+    //set parameter type
+    m_parameterType->setPrimitiveType(parameterType);
+
+    //check if siblingNode is not null
+    if (m_siblingNode != nullptr)
+    {
+        //set temp node to siblingNode
+        ParameterNode *node = dynamic_cast<ParameterNode*>(m_siblingNode);
+        
+        //set node type
+        node->setParameterType(parameterType);
+    }
+}
+
+//******************************************************
+// ************ VariableNode Class *********************
+//******************************************************
+
+//Constructor
+VariableNode::VariableNode(int tokenLineNumber, const std::string variableName, PrimitiveType *variableType, const bool isStaticVariable) : Node::Node(tokenLineNumber,variableName), m_variableType(variableType), m_isStaticVariable(isStaticVariable)
+{
+
+}
+
+//printTokenString
+std::string VariableNode::printTokenString() const
+{
+    if (m_variableType->getIsArray() && m_isStaticVariable)
+    {
+        return "Var: " + m_tokenData.stringValue + " of static array of type " + m_variableType->printTokenString();
+    }
+    else if (m_variableType->getIsArray())
+    {
+        return "Var: " + m_tokenData.stringValue + " of array of type " + m_variableType->printTokenString();
+    }
+    else if(m_isStaticVariable)
+    {
+        return "Var: " + m_tokenData.stringValue + " of static type " + m_variableType->printTokenString();
+    }
+    else
+    {
+        return "Var: " + m_tokenData.stringValue + " of type " + m_variableType->printTokenString();
+    }
+}
--- hw2/src/ast/DeclarationNodes.hpp
+++ hw2/src/ast/DeclarationNodes.hpp
@@ -17,14 +17,124 @@ DESC: DeclarationNodes Subclass. Inherits from Node Base Class.
 
 #include "Node.hpp"
 
-// Function, Parameters, Variable
-class DeclarationNodes : public Node
-{
-    public :
+// Function, Parameters, Primitives,  Variable
+// class DeclarationNodes : public Node
+// {
+//     public:
 
+//     //Declaration Type Enum
+//     enum class Type
+//     {
+//         FUNCTION,
+//         PARAMETER,
+//         VARIABLE,
+//     };
+
+//     //Constructor
+//     DeclarationNodes(int tokenLineNumber, const DeclarationNodes::Type declarationType);
+
+//     protected:
+// };
+
+class PrimitiveType
+{
+    public:
+
+        //Primitive Type Enum
+        enum class Type
+        {
+            INT,
+            CHAR,
+            BOOL,
+            VOID,
+        };
+
+        //Constructor
+        PrimitiveType(const PrimitiveType::Type type, const bool isArray = false);
+
+        //printTokenString
+        std::string printTokenString() const;
+
+        //setters
+
+        //setPrimitiveType
+        void setPrimitiveType(const PrimitiveType::Type type);
+
+
+        //getters
+        bool getIsArray() const;
+
+        Type getPrimitiveType() const;
     
 
-    protected :
+    protected:
+
+        PrimitiveType::Type m_type;
+
+        const bool m_isArray;
+
+        PrimitiveType::PrimitiveType *m_next;
+};
+
+class FunctionNode : public Node
+{
+    public:
+
+        //Constructor
+        FunctionNode(int tokenLineNumber, const std::string functionName, PrimitiveType *returnType);
+    
+        //printTokenString
+        std::string printTokenString() const;
+
+
+    protected:
+
+        PrimitiveType *m_returnType;
+}; 
+
+
+class ParameterNode : public Node
+{
+    public:
+
+        //Constructor
+        ParameterNode(int tokenLineNumber, const std::string parameterName, PrimitiveType *parameterType);
+    
+        //printTokenString
+        std::string printTokenString() const;
+
+        //setters
+        void setParameterType(PrimitiveType::Type parameterType);
+    
+
+    protected:
+
+        PrimitiveType *m_parameterType;
+};
+
+
+class VariableNode : public Node
+{
+    public:
+
+        //Constructor
+        VariableNode(int tokenLineNumber, const std::string variableName, PrimitiveType *variableType, const bool isStaticVariable = false);
+
+        //printTokenString
+        std::string printTokenString() const;
+
+        //set static variable
+        static void setStaticVariable();
+
+        //setters
+        void setVariableType(PrimitiveType *variableType);
+    
+
+    protected:
+
+        PrimitiveType *m_variableType;
+
+        bool  m_isStaticVariable;
 }; 
 
 
