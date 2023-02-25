a9585034f810f55b0b142c09c22a0f0d749f922a
Taylor <mart8517@vandals.uidaho.edu>
2023-02-25 12:04:54 -0800

    finished Semantics.hpp; working on semantics.cpp

--- hw3/src/Semantics/Semantics.cpp
+++ hw3/src/Semantics/Semantics.cpp
@@ -10,3 +10,230 @@ FILE: Semantics.hpp
 DESC: Semantics Class Definitions.  For semantic analysis of AST
 
 */
+
+#include "Semantics.hpp"
+
+
+
+// Constructor
+Semantics::Semantics(SymbolTable* symbolTable) : m_symbolTable{symbolTable}
+{
+
+}
+
+// Semantic Analysis
+void Semantics::analyze(Node* node)
+{
+    analyzeAST(node);
+
+    // Check for main function
+    if(!m_validMainExists)
+    {
+        EmitDiagnostics::Error::emitUndefinedMainError();
+    }
+}
+
+
+
+
+//********************Analyzers**********************
+void Semantics::analyzeAST(Node *node)
+{
+
+}
+
+//*********************Declaration Nodes**********************
+void Semantics::analyzeDeclarationNode(const DeclarationNode* declaration)
+{
+
+}
+
+
+void Semantics::analyzeFunctionNode(const Func* func)
+{
+
+}
+
+
+void Semantics::analyzeParmNode(const Parm* parm)
+{
+
+}
+
+
+void Semantics::analyzeVariableNode(Var* var)
+{
+
+}
+
+//*********************Expression Nodes**********************
+
+void Semantics::analyzeAsgn(const Asgn* asgn)
+{
+
+}
+
+
+void Semantics::analyzeBinaryNode(const Binary* binary) const
+{
+
+}
+
+
+void Semantics::analyzeCallNode(const Call* call) const
+{
+
+}
+
+
+void Semantics::analyzeExpressionNode(ExpressionNode* expression)
+{
+
+}
+
+
+void Semantics::analyzeIdentifierNode(const Id* id) const
+{
+
+}
+
+
+void Semantics::analyzeUnaryNode(const Unary* unary) const
+{
+
+}
+
+
+void Semantics::analyzeUnaryAsgn(const UnaryAsgn* unaryAsgn) const
+{
+
+}
+
+//*********************Statement Nodes**********************
+
+void Semantics::analyzeCompoundNode(const Compound* compound) const
+{
+
+}
+
+
+void Semantics::analyzeForNode() const
+{
+
+}
+
+
+void Semantics::analyzeReturnNode(const Return* returnNode) const
+{
+
+}
+
+
+void Semantics::analyzeStatementNode(const StatementNode* statement) const
+{
+
+}
+
+
+//********************Symbol Checkers**********************
+
+void Semantics::checkIndex(const Binary* binary) const
+{
+
+}
+
+
+void Semantics::checkOperandsOfSameType(ExpressionNode* expression) const
+{
+
+}
+
+
+void Semantics::checkOperandsOfType(ExpressionNode* expression, const NodeData::Type type) const
+{
+
+}
+
+
+void Semantics::checkUnaryOperands(const Unary* unary) const
+{
+
+}
+
+
+void Semantics::checkUnaryAsgnOperands(const UnaryAsgn* unaryAsgn) const
+{
+
+}
+
+
+//********************Helper Functions**********************
+
+bool Semantics::isMainFunc(const Func* func) const
+{
+
+}
+
+
+bool Semantics::isDeclared(const Id* id) const
+{
+
+}
+
+
+bool Semantics::hasIndexAncestor(const ExpressionNode* expression) const
+{
+
+}
+
+
+bool Semantics::hasAsgnAncestor(const ExpressionNode* expression) const
+{
+
+}
+
+
+bool Semantics::expOperandsExist(const ExpressionNode* expression) const
+{
+
+}
+
+
+bool Semantics::lhsExists(const ExpressionNode* expression) const
+{
+
+}
+
+
+std::string Semantics::getExpSym(const ExpressionNode* expression) const
+{
+
+}
+
+
+NodeData* Semantics::setAndGetExpData(const ExpressionNode* expression) const
+{
+
+}
+
+
+
+//********************Symbol Table Functions**********************
+
+
+void Semantics::leaveScope()
+{
+
+}
+
+
+bool Semantics::addToSymTable(const DeclarationNode* declaration, const bool global=false)
+{
+
+}
+
+
+DeclarationNode* Semantics::getFromSymTable(const std::string name) const
+{
+
+}
--- hw3/src/Semantics/Semantics.hpp
+++ hw3/src/Semantics/Semantics.hpp
@@ -8,4 +8,88 @@ Dr. Wilder
 
 FILE: Semantics.hpp
 DESC: Semantics Class Declaration.  For semantic analysis of AST
+
 */
+#ifndef SEMANTICS_HPP
+#define SEMANTICS_HPP
+
+#include "symbolTable.hpp"
+#include "ASTNodeIdentifier.hpp"
+#include "../Emit/EmitDiagnostics.hpp"
+#include "../AST/AST.hpp"
+
+#include <string>
+#include <sstream>
+
+class Semantics
+{
+    public: 
+
+        // Constructor
+        Semantics(SymbolTable* symbolTable);
+
+        // Semantic Analysis
+        void analyze(Node* node);
+
+
+    protected:
+
+        //********************Analyzers**********************
+        void analyzeAST(Node *node);
+        
+        //*********************Declaration Nodes**********************
+        void analyzeDeclarationNode(const DeclarationNode* declaration);
+        void analyzeFunctionNode(const Func* func);
+        void analyzeParmNode(const Parm* parm);
+        void analyzeVariableNode(Var* var);
+        
+        //*********************Expression Nodes**********************
+        void analyzeAsgn(const Asgn* asgn);
+        void analyzeBinaryNode(const Binary* binary) const;
+        void analyzeCallNode(const Call* call) const;
+        void analyzeExpressionNode(ExpressionNode* expression);
+        void analyzeIdentifierNode(const Id* id) const;
+        void analyzeUnaryNode(const Unary* unary) const;
+        void analyzeUnaryAsgn(const UnaryAsgn* unaryAsgn) const;
+        
+        //*********************Statement Nodes**********************
+        void analyzeCompoundNode(const Compound* compound) const;
+        void analyzeForNode() const;
+        void analyzeReturnNode(const Return* returnNode) const;
+        void analyzeStatementNode(const StatementNode* statement) const;
+
+
+        //********************Symbol Checkers**********************
+        void checkIndex(const Binary* binary) const;
+        void checkOperandsOfSameType(ExpressionNode* expression) const;
+        void checkOperandsOfType(ExpressionNode* expression, const NodeData::Type type) const;
+        void checkUnaryOperands(const Unary* unary) const;
+        void checkUnaryAsgnOperands(const UnaryAsgn* unaryAsgn) const;
+
+
+        //********************Helper Functions**********************
+        bool isMainFunc(const Func* func) const;
+        bool isDeclared(const Id* id) const;
+        bool hasIndexAncestor(const ExpressionNode* expression) const;
+        bool hasAsgnAncestor(const ExpressionNode* expression) const;
+        bool expOperandsExist(const ExpressionNode* expression) const;
+        bool lhsExists(const ExpressionNode* expression) const;
+        std::string getExpSym(const ExpressionNode* expression) const;
+        NodeData* setAndGetExpData(const ExpressionNode* expression) const;
+
+
+
+        //********************Symbol Table Functions**********************
+        void leaveScope();
+        bool addToSymTable(const DeclarationNode* declaration, const bool global=false);
+        DeclarationNode * getFromSymTable(const std::string name) const;
+
+
+        //********************Data Members**********************
+        SymbolTable* m_symbolTable;
+        Node* m_root;
+        bool m_validMainExists = false;
+
+}; 
+
+#endif // SEMANTICS_HPP
